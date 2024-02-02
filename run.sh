ff7bcbdf63999fe150ce13aef4e4cd5c1f672fa6
yauhenAleo <152736554+YauhenAleo@users.noreply.github.com>
2024-02-02 22:52:39 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,4 @@
-#!/bin/bash
-# First check that Leo is installed.
-if ! command -v leo &> /dev/null
-then
-    echo "leo is not installed."
-    exit
-fi
-# Create a new game.
-echo "
-###############################################################################
-########                                                               ########
-########          STEP 0: Creating a new game of Tic-Tac-Toe           ########
-########                                                               ########
-########                         |   |   |   |                         ########
-########                         |   |   |   |                         ########
-########                         |   |   |   |                         ########fi
-# Create a new game.
+
 echo "
 ###############################################################################
 ########                                                               ########
